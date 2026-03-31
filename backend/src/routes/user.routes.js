6e7ff7a2cfdd3c5b1c2c9059e9f46a459c5b7724
Harsh Upadhyay <uharsh328@gmail.com>
2026-03-31 20:15:07 +0530

    feat: integrate multer middleware for file uploads in user registration route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,10 @@
 import {Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
 const router = Router();
+import {upload } from "../middlewares/multer.middleware.js";
 
-router.route('/register').post(registerUser);
+router.route('/register').post(
+    upload.any(),
+    registerUser);
 
 export default router;
