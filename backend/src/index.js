be955fe56e848325bf84f29bc840f65f76cbadcc
Harsh Upadhyay <uharsh328@gmail.com>
2026-03-31 20:14:21 +0530

    feat: move app initialization to the top of index.js for better readability

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 
 
-const app = express();
+
 dotenv.config({
     path: './.env'
 });
