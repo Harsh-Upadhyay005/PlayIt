800005241e189eeeed6a7ecd6f1a62d6b321494f
Harsh Upadhyay <uharsh328@gmail.com>
2026-03-31 20:14:52 +0530

    refactor: simplify multer storage configuration by using memory storage

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,4 @@
 import multer from 'multer';
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp");
-    }, 
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
-
-    }
-})
-
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage });
