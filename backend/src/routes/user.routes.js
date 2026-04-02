4c9a4d57a86f0c9f9a5e73af5e254f8ab2264b11
Harsh Upadhyay <uharsh328@gmail.com>
2026-04-02 23:04:10 +0530

    feat: update file upload handling in user registration route to support multiple fields

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,29 @@
 import {Router } from 'express';
 import { registerUser } from '../controllers/user.controller.js';
-const router = Router();
 import {upload } from "../middlewares/multer.middleware.js";
+import { loginUser } from '../controllers/auth.controller.js';
+import { verifyJwT } from '../middlewares/auth.middleware.js';
+
+const router = Router();
 
 router.route('/register').post(
-    upload.any(),
-    registerUser);
+    
+    upload.fields([
+        {
+            name: 'profilePicture',
+            maxCount: 1,
+        },
+        {
+            name: 'coverPicture',
+            maxCount: 1,
+        }
+    ]),
+    registerUser
+)
+
+router.route('/login').post(loginUser);
+
+// secured route - only for authenticated users
+router.route('/logout').post(verifyJwT, logoutUser);
 
 export default router;
