7c317335eee0e1c39839d05bfbb4d59f55d98070
Harsh Upadhyay <uharsh328@gmail.com>
2026-04-02 23:04:16 +0530

    feat: add JWT verification middleware for user authentication

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import asyncHandler from "../utils/asyncHandlers";
+import ApiError from "../utils/ApiError.js";
+import jwt from 'jsonwebtoken';
+import { User } from "../models/user.models";
+
+export const verifyJwT = asyncHandler( async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.headers?.authorization?.replace('Bearer ', '');
+        if(!token) {
+            throw new ApiError(401, 'Unauthorized: No token provided');
+        }
+        const decodedToken = jwt.verify(token, process.env.Access_Token_Secret)
+    
+        const user = await User.findById(decodedToken._id).select('-password -refreshToken')
+        if (!user) {
+            throw new ApiError(404, 'User not found');
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error.message || 'Unauthorized: Invalid token');
+    }
+});
+   
