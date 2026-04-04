6cb597f30f14bf5d198a95eaf023ca332eb30de6
Harsh Upadhyay <uharsh328@gmail.com>
2026-04-04 15:06:49 +0530

    fix: correct import statements and function name in JWT verification middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
-import asyncHandler from "../utils/asyncHandlers";
+import asyncHandler from "../utils/asyncHandlers.js";
 import ApiError from "../utils/ApiError.js";
 import jwt from 'jsonwebtoken';
-import { User } from "../models/user.models";
+import { User } from "../models/user.models.js";
 
-export const verifyJwT = asyncHandler( async (req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.headers?.authorization?.replace('Bearer ', '');
         if(!token) {
             throw new ApiError(401, 'Unauthorized: No token provided');
         }
-        const decodedToken = jwt.verify(token, process.env.Access_Token_Secret)
+        const decodedToken = jwt.verify(token, process.env.Access_Token_Secret);
     
-        const user = await User.findById(decodedToken._id).select('-password -refreshToken')
+        const user = await User.findById(decodedToken._id).select('-password -refreshToken');
         if (!user) {
             throw new ApiError(404, 'User not found');
         }
